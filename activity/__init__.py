00a57b3b9963966149f20432bdaac4ca3facd7ba
eofs <tomi@madlab.fi>
2015-03-30 12:06:33 +0300

    Automatically discover activity registries.
    Use AppConfiguration to automatically load activity registries. Requires Django 1.7 or later.

--- activity/__init__.py
+++ activity/__init__.py
@@ -1,6 +1,12 @@
+from django.utils.module_loading import autodiscover_modules
+
 __version__ = '0.3.1'
 
 try:
     from activity.signals import action
 except ImportError:
     pass
+
+
+def autodiscover():
+    autodiscover_modules('activity_registry')
---
+++ activity/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
+
+
+class ActivityConfig(AppConfig):
+    name = 'activity'
+    verbose_name = _("Activity")
+
+    def ready(self):
+        self.module.autodiscover()
